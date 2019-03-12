aae748e13cc051671e81b5e864607121a8093359
totalhack <none@none.com>
2019-03-12 17:20:53 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+PACKAGE := sqlaw
+ENV := /usr
+PIP := $(ENV)/bin/pip
+SETUP := $(ENV)/bin/python setup.py
+VERSION := $(shell echo `date +%Y%m%d%H%M%S`)
+
+clean:
+	rm -rf build dist *.egg-info
+
+develop:
+	$(PIP) install -U -e ./ --no-binary ":all:"
+
+install:
+	$(SETUP) bdist_wheel egg_info --tag-build '.$(VERSION)'
+	$(PIP) install dist/$(PACKAGE)-0.0.$(VERSION)-py3-none-any.whl
+
+uninstall:
+	if ($(PIP) freeze 2>&1 | grep $(PACKAGE)); \
+		then $(PIP) uninstall $(PACKAGE) --yes; \
+	else \
+		echo 'Package not installed'; \
+	fi
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
+
+def get_readme_text():
+    f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+    text = f.read()
+    f.close()
+    return text
+
+setup(
+    name='sqlaw',
+    long_description=get_readme_text(),
+    author='totalhack',
+    maintainer='totalhack',
+    version='0.0',
+    classifiers=['License :: MIT License',
+                 'Programming Language :: Python :: 3',
+                 ],
+    packages=find_packages(),
+    include_package_data=True,
+    )
