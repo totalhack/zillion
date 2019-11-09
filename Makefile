5fd799665251a16e7c558a478aa8b464e3454dea
totalhack <none@none.com>
2019-11-08 19:12:22 -0500

    Update package files

--- Makefile
+++ Makefile
@@ -1,22 +1,60 @@
-PACKAGE := zillion
-ENV := /usr
-PIP := $(ENV)/bin/pip
-SETUP := $(ENV)/bin/python setup.py
-VERSION := $(shell echo `date +%Y%m%d%H%M%S`)
+ENV := $(HOME)/env/zillion
+TEST_ENV := /tmp/zillion_pip_test/
+PACKAGE_NAME := 'zillion'
+VERSION := $(shell python setup.py --version)
+EGG_OPTIONS := egg_info
+PIP_CMD := $(ENV)/bin/pip
+SETUP_CMD := $(ENV)/bin/python setup.py
+
+all: install
 
 clean:
 	rm -rf build dist *.egg-info
 
 develop:
-	$(PIP) install -U -e ./ --no-binary ":all:"
+	$(PIP_CMD) install -U -e ./ --no-binary ":all:"
 
 install:
-	$(SETUP) bdist_wheel egg_info --tag-build '.$(VERSION)'
-	$(PIP) install dist/$(PACKAGE)-0.0.$(VERSION)-py3-none-any.whl
+	$(SETUP_CMD) bdist_wheel $(EGG_OPTIONS)
+	$(PIP_CMD) install -U dist/$(PACKAGE_NAME)-$(VERSION)-py3-none-any.whl
 
 uninstall:
-	if ($(PIP) freeze 2>&1 | grep $(PACKAGE)); \
-		then $(PIP) uninstall $(PACKAGE) --yes; \
+	if ($(PIP_CMD) freeze 2>&1 | grep $(PACKAGE_NAME)); \
+		then $(PIP_CMD) uninstall $(PACKAGE_NAME) --yes; \
 	else \
-		echo 'Package not installed'; \
+		echo 'No installed package found!'; \
 	fi
+
+dist:
+	$(MAKE) clean
+	$(SETUP_CMD) sdist bdist_wheel
+
+upload:
+	$(ENV)/bin/python -m twine upload dist/*
+
+test_upload:
+	$(ENV)/bin/python -m twine upload --repository-url "https://test.pypi.org/legacy/" dist/*
+
+test_env:
+	rm -rf $(TEST_ENV)
+	mkdir $(TEST_ENV)
+	$(ENV)/bin/python	-m venv $(TEST_ENV)
+	$(TEST_ENV)/bin/pip install -U pip
+
+pip:
+	$(MAKE) dist
+	$(MAKE) upload
+	$(MAKE) test_env
+	sleep 30
+	$(TEST_ENV)/bin/pip install -U zillion==$(VERSION)
+	$(TEST_ENV)/bin/python -c "import zillion"
+
+test_pip:
+	$(MAKE) dist
+	$(MAKE) test_upload
+	$(MAKE) test_env
+	sleep 30
+	$(TEST_ENV)/bin/pip install -i "https://test.pypi.org/simple/" --extra-index-url "https://pypi.org/simple/" zillion==$(VERSION)
+	$(TEST_ENV)/bin/python -c "import zillion"
+
+.PHONY: dist clean test_env
--- setup.py
+++ setup.py
@@ -1,22 +1,82 @@
 #!/usr/bin/env python
+
+from contextlib import contextmanager
 import os
+
 from setuptools import find_packages, setup
 
-def get_readme_text():
-    f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-    text = f.read()
-    f.close()
-    return text
+
+def find_deploy_scripts(path, include_patterns, exclude_patterns=[]):
+    cmd = "FILES=`find %s -path %s" % (path, (" -o -path ").join(include_patterns))
+    if exclude_patterns:
+        cmd += " | grep -v -E '(%s)'" % ("|").join(exclude_patterns)
+    cmd += "`;"
+    cmd += " for FILE in $FILES; do if [ `echo $FILE | xargs grep -l '/usr/bin/env python'` ] || [ `echo $FILE | grep -v .py` ]; then echo $FILE; fi; done"
+    h = os.popen(cmd)
+    out = h.read()
+    h.close()
+    return out.split()
+
+
+@contextmanager
+def load_file(fname):
+    f = open(os.path.join(os.path.dirname(__file__), fname))
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+with load_file("README.md") as f:
+    README = f.read()
+
+with load_file("requirements.txt") as f:
+    requires = f.read().split("\n")
+
+# Split git requirements to fill in dependency_links
+git_requires = [x for x in requires if "git" in x]
+non_git_requires = [x for x in requires if "git" not in x]
+for repo in git_requires:
+    # Append git egg references
+    non_git_requires.append(repo.split("egg=")[-1])
+
+
+extras_require = {
+    "dev": [
+        "black",
+        "pre-commit",
+        "pylint",
+        "pytest",
+        "sphinx",
+        "twine",
+        "wheel",
+    ],
+}
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
+
+exec(open("zillion/version.py").read())
 
 setup(
-    name='zillion',
-    long_description=get_readme_text(),
-    author='totalhack',
-    maintainer='totalhack',
-    version='0.0',
-    classifiers=['License :: MIT License',
-                 'Programming Language :: Python :: 3',
-                 ],
-    packages=find_packages(),
+    name="zillion",
+    description="Make sense of it all.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/totalhack/zillion",
+    author="totalhack",
+    author_email="none@none.com",
+    maintainer="totalhack",
+    version=__version__,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    scripts=find_deploy_scripts("zillion", ["\\*.py", "\\*.sh", "\\*.sql"], ["__init__"]),
+    packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
-    )
+    install_requires=non_git_requires,
+    dependency_links=git_requires,
+    extras_require=extras_require,
+)
